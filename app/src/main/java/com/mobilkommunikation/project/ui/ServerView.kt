4cf702d407069aa43c8e4e6e8a689bfa9152a849
Alexander Kleinwächter <alexander.kleinwaechter@stud.uni-bamberg.de>
2024-11-20 18:45:21 +0100

    readme.md += content

--- app/src/main/java/com/mobilkommunikation/project/ui/ServerView.kt
+++ app/src/main/java/com/mobilkommunikation/project/ui/ServerView.kt
@@ -1,11 +1,19 @@
 package com.mobilkommunikation.project.ui
 
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.mobilkommunikation.project.ui.components.OutputField
 
 @Composable
-fun PacketWatcherServerView() {
-    Text(
-        text = "To Do: Not yet implemented"
-    )
+fun PacketWatcherServerView(
+    // parameters to come
+) {
+    Column {
+        Spacer(modifier = Modifier.height(16.dp))
+        OutputField(outputText = "Display Output here")
+    }
 }
